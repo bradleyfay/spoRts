56d82319f780c9490355fdd28eeea0e2d8a7f9c8
Bradley Fay <bradley.fay@gmail.com>
2017-04-12 07:38:27 -0400

    Add documentation and flexible args for nba_game_logs

--- R/nba_game_logs.R
+++ R/nba_game_logs.R
@@ -1,16 +1,27 @@
-nba.LeagueGameLog <- function(season) {
+#' Get Game Logs
+#'
+#' @param ...
+#'
+#' @return dataframe
+#' @export
+#'
+#' @examples
+#' df <- nba.LeageGameLog()
+#' head(df)
+nba.LeagueGameLog <- function(...) {
   URL <- gen_url("leaguegamelog")
-
-  params <- list(Season = '2016-17',
-                 LeagueID = '00',
-                 PlayerOrTeam = 'T',
-                 Direction = 'DESC',
-                 SeasonType = 'Regular Season',
-                 Sorter = 'FGM',
-                 DateFrom = NULL,
-                 DateTo = '2017-04-10',
-                 Counter = 0)
-
+    params <- list(
+      Season = '2016-17',
+      LeagueID = '00',
+      PlayerOrTeam = 'T',
+      Direction = 'DESC',
+      SeasonType = 'Regular Season',
+      Sorter = 'FGM',
+      DateFrom = NULL,
+      DateTo = Sys.Date() - 1,
+      # Defaulting to yesterday as it gets wonky with games still in play
+      Counter = 0)
+  params <- modifyList(...)
   resp <- httr::GET(url = URL, query = params, nba_hdrs)
   content <- jsonlite::fromJSON(httr::content(resp, "text", encoding = "UTF-8"),
                                 simplifyVector = FALSE)
