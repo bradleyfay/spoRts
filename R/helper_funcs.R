39ce41e632e1a79216b7c11b8e233a7ecd5ccb35
Bradley <bradley.fay@gmail.com>
2017-04-22 08:46:55 -0400

    Add Season Generator Helper Function

--- R/helper_funcs.R
+++ R/helper_funcs.R
@@ -9,3 +9,19 @@ create_df <- function(results, tbl_id){
   u <- purrr::map(t, unlist)
   return(as.data.frame(u, col.names = unlist(results$resultSets[[tbl_id]]$headers)))
 }
+
+get_season <- function(date = Sys.Date()) {
+
+  if (!inherits(date, "Date")) {
+    # Add Error Handling to ensure a Date object gets passed
+    stop()
+  }
+
+  month <- as.integer(format(date, "%m"))
+  year <- as.integer(format(date, "%Y"))
+  if (month < 10) {
+    sprintf("%d-%02d", year - 1, year %% 100)
+  } else {
+    sprintf("%d-%02d", year, (year + 1) %% 100)
+  }
+}
